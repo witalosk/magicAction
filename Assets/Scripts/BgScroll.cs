dd502c84ec14e27d82a9301c95eaa7f640af97cf
Yusuke57 <honey57g@gmail.com>
2019-02-04 01:58:23 +0900

    [add] 背景スクロール処理と風の背景画像を作成

--- Assets/Scripts/BgScroll.cs
+++ Assets/Scripts/BgScroll.cs
@@ -2,33 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BgManager : MonoBehaviour {
+public class BgScroll : MonoBehaviour {
 
-	Transform bgTrans;
+	Transform bgParentTrans;
 	Transform cameraTrans;
 	Transform playerTrans;
 
 	[SerializeField] float speed;
 	Vector3 prePos;
 
+
+	Transform[] bgTranses;
+	float spriteWidth;
+	int centerSpriteIdx;
+
 	void Awake(){
-		bgTrans = transform;
-		prePos = Vector3.zero;
+		bgParentTrans = GetComponentInChildren<Transform>();
+		bgTranses = new Transform[3];
+		for(int i = 0; i < 3; i++)
+			bgTranses[i] = bgParentTrans.GetChild(i);
+		spriteWidth = bgTranses[0].gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
+		centerSpriteIdx = 1;
 	}
 
 	void Start(){
-		if(speed == 0)
-			cameraTrans = Camera.main.transform;
-		else
-			playerTrans = GameObject.FindGameObjectWithTag ("Player").transform;
+		playerTrans = GameObject.FindGameObjectWithTag ("Player").transform;
+		prePos = playerTrans.position;
 	}
 
 	void LateUpdate(){
-		if (speed == 0) {
-			bgTrans.position = cameraTrans.position + Vector3.forward * 20;
-		}else{
-			bgTrans.position += (playerTrans.position - prePos) * speed;
-			prePos = playerTrans.position;
+		// bgParentTrans.position += Vector3.right * (playerTrans.position.x - prePos.x) * speed;
+		bgParentTrans.position += (playerTrans.position - prePos) * speed;
+		// bgParentTrans.position += Vector3.up * (playerTrans.position.y - prePos.y);
+		prePos = playerTrans.position;
+
+		CheckSeamless();
+	}
+
+	void CheckSeamless(){
+
+		float diff = playerTrans.position.x - bgTranses[centerSpriteIdx].position.x;
+		if(diff > spriteWidth / 2){
+			bgTranses[(centerSpriteIdx + 2) % 3].position += Vector3.right * spriteWidth * 3;
+			centerSpriteIdx = (centerSpriteIdx + 1) % 3;
+		}else if(diff < -spriteWidth / 2){
+			bgTranses[(centerSpriteIdx + 1) % 3].position -= Vector3.right * spriteWidth * 3;
+			centerSpriteIdx = (centerSpriteIdx + 2) % 3;
 		}
 	}
 }
