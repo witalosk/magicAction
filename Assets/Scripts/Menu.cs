3a753eefeee8e2e76f129b929b7635012fc6b9ec
yuyurun <y.hashizume@mibel.cs.tsukuba.ac.jp>
2019-02-04 02:41:26 +0900

    [add] main menuの作成・画面遷移

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject SelectWorlds;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        MainMenu.SetActive(false);
+        SelectWorlds.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
