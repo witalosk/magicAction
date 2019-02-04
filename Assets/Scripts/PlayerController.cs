627f5c22be05fe716e3a11e0011c88c40b3ce5e9
witalosk <shun_september@live.jp>
2019-02-04 23:31:23 +0900

    [add] プレーヤーにアニメを適用，向きによってグラフィックの向き変更

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,64 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	Transform playerTrans;
-	Rigidbody2D playerRB;
+    Transform playerTrans;
+    Rigidbody2D playerRB;
 
-	[SerializeField] float playerMoveSpeed;
-	[SerializeField] float jumpPower;
+    [SerializeField] float playerMoveSpeed;
+    [SerializeField] float jumpPower;
 
-	[SerializeField] float gravityForce;
+    [SerializeField] float gravityForce;
 
-	[SerializeField] LayerMask groundCheckLM;
+    [SerializeField] LayerMask groundCheckLM;
 
-	bool isGround;
-	bool checkGround;
+    bool isGround;
+    bool checkGround;
 
-	void Awake(){
-		playerTrans = transform;
-		playerRB = GetComponent<Rigidbody2D> ();
-	}
-
-	public void Start(){
-		isGround = true;
-		checkGround = false;
-	}
+    // アニメーション用
+    private Animator animator;
 
 
-	void Update () {
-		if(!isGround && checkGround){
-			RaycastHit2D hit = Physics2D.Raycast (playerTrans.position, Vector2.down, 1.2f, groundCheckLM);
-			if(hit) isGround = true;
-		}
+    void Awake()
+    {
+        playerTrans = transform;
+        playerRB = GetComponent<Rigidbody2D>();
+    }
 
-		float dir = Input.GetAxisRaw("Horizontal");
-		if (Mathf.Abs(dir) > 0.01f)
-			PlayerMove (dir);
-		if (Input.GetKeyDown (KeyCode.UpArrow))
-			PlayerJump ();
-	}
+    public void Start()
+    {
+        isGround = true;
+        checkGround = false;
 
-	void FixedUpdate(){
-		playerRB.AddForce(Vector2.down * gravityForce);
-	}
+        animator = GetComponent<Animator>(); // アニメータの取得
+    }
 
-	void PlayerMove(float dir){
-		playerTrans.position += Vector3.right * dir * playerMoveSpeed * Time.deltaTime;
-	}
 
-	void PlayerJump(){
-		if (isGround) {
-			playerRB.velocity = new Vector3(playerRB.velocity.x, 0, 0);
-			playerRB.AddForce (Vector2.up * jumpPower);
-			isGround = false;
-			checkGround = false;
-			Invoke("EnableCheckGround", 0.2f);
-		}
-	}
+    void Update()
+    {
+        if (!isGround && checkGround)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(playerTrans.position, Vector2.down, 2.0f, groundCheckLM);
+            if (hit) isGround = true;
+        }
+        animator.SetBool("is_running", false);
 
-	void EnableCheckGround(){
-		checkGround = true;
-	}
+        float dir = Input.GetAxisRaw("Horizontal");
+        if (Mathf.Abs(dir) > 0.01f)
+            PlayerMove(dir);
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            PlayerJump();
+    }
+
+    void FixedUpdate()
+    {
+        playerRB.AddForce(Vector2.down * gravityForce);
+    }
+
+    void PlayerMove(float dir)
+    {
+        animator.SetBool("is_running", true);
+        playerTrans.position += Vector3.right * dir * playerMoveSpeed * Time.deltaTime;
+
+        // プレーヤーの描画向き変更
+        if (dir > 0) playerTrans.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+        else playerTrans.localScale = new Vector3(-2.0f, 2.0f, 2.0f);
+    }
+
+    void PlayerJump()
+    {
+        if (isGround)
+        {
+            animator.SetTrigger("is_jumping");
+            playerRB.velocity = new Vector3(playerRB.velocity.x, 0, 0);
+            playerRB.AddForce(Vector2.up * jumpPower);
+            isGround = false;
+            checkGround = false;
+            Invoke("EnableCheckGround", 0.2f);
+        }
+    }
+
+    void EnableCheckGround()
+    {
+        checkGround = true;
+    }
 }
